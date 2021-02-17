834254ffc423baaca4c6e6803037807b3c6e30d4
YasH <yashdesoi@gmail.com>
2021-02-17 18:50:24 +0530

    minor changes

--- 15) Babel and Webpack/src/index.js
+++ 15) Babel and Webpack/src/index.js
@@ -2,8 +2,14 @@
 import { camelCase } from 'lodash';
 
 // Importing our own module
+
+// Method 1
 import userData, {email as userEmail, add} from './module';
 
+// Importing everything
+// import * as myModule from './module';
+
+// Use case of method 1
 console.log(camelCase('yash desai'));
 
 console.log(userData);
@@ -11,3 +17,14 @@ console.log(userData);
 console.log(userEmail);
 
 console.log(add(5,8));
+
+// Use case of myModule
+// console.log(myModule);
+
+// console.log(myModule.default);
+
+// console.log(myModule.sub(5,9));
+
+// console.log(myModule.email);
+
+
