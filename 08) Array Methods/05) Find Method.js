fec65e5d65d1ff8caa86ae929fb6708ee1b4f946
yashdesoi <yashdesoi@gmail.com>
2021-05-25 13:06:27 +0530

    minor updates

--- 08) Array Methods/05) Find Method.js
+++ 08) Array Methods/05) Find Method.js
@@ -1,4 +1,4 @@
-// array.find(callbackFunction()) returns the value of the first element inside an array which passes a certain test in callback function.
+// array.find(callbackFunction()) returns the value by referance of the first element inside an array which passes a certain test in callback function.
 
 // EXAMPLE 1
 let scores = [10, 5, 0, 40, 60, 10, 20, 70];
@@ -16,5 +16,27 @@ console.log(firstHighestScore);
 scores = [10, 61, 0, 40, 60, 10, 20, 70];
 
 firstHighestScore = scores.find(score => score > 50);
-
 console.log(firstHighestScore);
+
+firstHighestScore = scores.find(score => score > 101); // If it cannot find it will return undefined
+console.log(firstHighestScore);
+
+
+
+
+// Example 3 (Value is passed by reference)
+serverList = [
+    { id: 1, name: 'Dev', status: 'online' },
+    { id: 2, name: 'Production', status: 'offline' },
+];
+
+const server = serverList.find(server => {
+    return server.id === 2;
+});
+
+console.log(server);
+
+server.status = 'online';
+
+console.table(serverList);
+
